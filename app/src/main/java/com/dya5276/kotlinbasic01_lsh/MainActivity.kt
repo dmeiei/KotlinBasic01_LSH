5d914af202de981b2da084206765de7309a31ce0
Lee SeungHyun <leesh0545@gmail.com>
2022-07-28 17:56:05 +0900

    log 찍어보기

--- app/src/main/java/com/dya5276/kotlinbasic01_lsh/MainActivity.kt
+++ app/src/main/java/com/dya5276/kotlinbasic01_lsh/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dya5276.kotlinbasic01_lsh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        smallBtn
+        clickBtn.setOnClickListener {
+
+            //주석 ctrl+/: 커서에 있는 줄을 주석처리
+//                이{}안에 있는 코드만, 버튼이 눌렸을 때 실행됨
+
+            Log.d("메인화면로그", "클릭용 버튼이 눌림")
+
+        }
 
     }
 
